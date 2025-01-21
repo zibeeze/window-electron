2da648d3971ca4bfb406f1763bf253a0ca9ec6c7
Alexander Schantz <alex.schantz@gmail.com>
2025-01-21 16:11:08 -0500

    maker deb

--- forge.config.ts
+++ forge.config.ts
@@ -1,9 +1,9 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
+// import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+// import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
-import { MakerSnap } from "@electron-forge/maker-snap";
+// import { MakerRpm } from "@electron-forge/maker-rpm";
+// import { MakerSnap } from "@electron-forge/maker-snap";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
@@ -17,7 +17,7 @@ const config: ForgeConfig = {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [new MakerSnap({})],
+  makers: [new MakerDeb({})],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
