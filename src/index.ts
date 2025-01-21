5bc72d320221bad007edecd0931af0773c70670b
Alexander Schantz <alex.schantz@gmail.com>
2025-01-21 16:32:44 -0500

    kiosk

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const createWindow = (): void => {
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
-    // kiosk: true,
+    kiosk: true,
   });
 
   // and load the index.html of the app.
